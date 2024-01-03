92ab7fb2c3980c11def396f25155f8f5f515e42f
Francesco Pizzino <ciccio@MBPdiFrancesco.lan>
2024-01-03 15:18:56 +0100

    First Commit(first exercise)

---
+++ script.js
@@ -0,0 +1,13 @@
+// Dichiaro la variabile 
+
+let box = document.getElementById("box");
+
+// Definisco la funzione
+
+function changeColor() {
+    box.style.backgroundColor = "blue";
+}
+
+// Eseguo la funzione
+
+changeColor();
