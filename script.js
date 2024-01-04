079bb211e01cb5178b20483efffe96e6e449c2f6
Francesco Pizzino <ciccio@MBPdiFrancesco.lan>
2024-01-04 16:24:58 +0100

    feat(esercizi natale): Added exercise 9

--- script.js
+++ script.js
@@ -1,19 +1,7 @@
+let text = document.getElementById("text");
 
-// Per dichirare le variabili, mi assicuro questa volta di dichiararle all'interno di una funzione che si assicura di scaricare il javascript soltanto dopo aver caricato tutto il DOM sul browser.
-document.addEventListener('DOMContentLoaded', function () {
+function changeColor() {
+    text.style.color = "blue";
+}
 
-    // Inglobo tutto il mio codice all'interno di una funzione anonima. 
-
-    let red = document.getElementById('red');
-    let black = document.getElementById('black');
-
-    function changeWidthHeight() {
-        red.style.width = "100px";
-        red.style.height = "100px";
-        black.style.width = "100px";
-        black.style.height = "100px";
-    }
-
-    red.addEventListener('click', changeWidthHeight);
-    black.addEventListener('click', changeWidthHeight);
-});
+text.addEventListener("click", changeColor);
