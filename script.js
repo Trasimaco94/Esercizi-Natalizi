7ac1161c571dee06bcb46bf18e10b2bbc3220547
Francesco Pizzino <ciccio@MBPdiFrancesco.lan>
2024-01-10 17:32:03 +0100

    feat(esercizi natale): Added exercise 23

--- script.js
+++ script.js
@@ -1,32 +1,15 @@
-const previous = document.getElementById('previous');
-const next = document.getElementById('next');
-const images = document.querySelectorAll('.box img');
+// Dichiaro le variabili
+let box = document.getElementById('box');
 
+// Dichiaro la funzione
+function transparent () {
 
-let currentIndex = 0;
+    box.style.opacity = 0.3;
 
-function showImage(index) {
-    // Nascondo tutte le immagini
-    images.forEach(image => image.style.display = 'none');
-
-    // Mostro solo l'immagine corrente
-    images[index].style.display = 'block';
 }
 
-function enhanceImg(direction) {
-    // Avanti o Indietro
-    if (direction === 'next') {
-        currentIndex = (currentIndex + 1) % images.length;
-    } else {
-        currentIndex = (currentIndex - 1 + images.length) % images.length;
-    }
+// La proprietà opacity accetta valori compresi tra 0 e 1, dove 0 è completamente trasparente e 1 è completamente opaco.
 
-    showImage(currentIndex);
-}
+// Eseguo
+box.addEventListener('click', transparent);
 
-// Mostro la prima immagine all'inizio
-showImage(currentIndex);
-
-// Aggiungo gli eventi ai pulsanti
-previous.addEventListener('click', () => enhanceImg('previous'));
-next.addEventListener('click', () => enhanceImg('next'));
